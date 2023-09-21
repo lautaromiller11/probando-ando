a47bc8669daec30d5acf033f14df6be4829841d7
Lautaro Miller <lautaromiller11@gmail.com>
2023-09-21 20:26:06 -0300

    first commit

--- prueba.java
+++ prueba.java
@@ -3,6 +3,6 @@ package probando;
 public class Probando {
 
     public static void main(String[] args) {
-        System.out.println("Probando Jenkins");
+        System.out.println("A commit has been made");
     }
 }
